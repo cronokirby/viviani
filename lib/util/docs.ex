2ce3326c2878a782f550fb780a7ef3362533276f
cronokirby <cronokirby@gmail.com>
2017-04-01 23:02:41 +0200

    adds the ability to provide suggestions for typos when using fun_header/3

--- lib/util/docs.ex
+++ lib/util/docs.ex
@@ -10,13 +10,31 @@ defmodule Viviani.Util.Docs do
     end
   end
 
+  @doc """
+  Gets the first line of a function doc.
+
+  ## Returns
+  - `nil`
+    The module doesn't exist.
+  - `{:similar, suggestions}`
+    Returns a list of `{function, arity}` pairs of closely matching function names.
+  - `{:ok, text}`
+    This is a succesful header.
+  """
   def fun_header(module, fun, arity) do
     with [_|_] = docs <- Code.get_docs(module, :docs) do
       case Enum.filter(docs, &match?({{^fun, ^arity}, _, _, _, _}, &1)) do
-        # conforming to the previous return
-        [] -> nil
+        # Sort the results by jaro distance, take the 3 closest
+        [] ->
+          docs
+          |> Stream.map(fn {info, _, _, _, _} -> info end)
+          |> Enum.sort_by(fn {f, _} ->
+            String.jaro_distance(Atom.to_string(fun), Atom.to_string(f))
+          end, &>=/2)
+          |> Enum.take(3)
+          |> (& {:similar, &1}).()
         [{_, _, _, _spec, text}] ->
-          hd String.split(text, "\n")
+          {:ok, hd String.split(text, "\n")}
       end
     end
   end
--- test/util/docs_test.exs
+++ test/util/docs_test.exs
@@ -18,8 +18,14 @@ defmodule Viviani.Util.DocsTest do
 
   @cache_emoji "Retrieves a custom emoji by id in a guild."
 
-  test "Function headers should be accessible, or nil" do
-    assert Docs.fun_header(Alchemy.Cache, :emoji, 2) == @cache_emoji
-    assert Docs.fun_header(Enum, :each, 2) == @enum_each
+  test "Function headers should be accessible" do
+    assert Docs.fun_header(Alchemy.Cache, :emoji, 2) == {:ok, @cache_emoji}
+    assert Docs.fun_header(Enum, :each, 2) == {:ok, @enum_each}
+  end
+
+  test "Function headers should yield suggestions" do
+    assert {:similar, [_, _, _]} = Docs.fun_header(Alchemy.Cache, :mmeber, 2)
+    {:similar, suggestions} = Docs.fun_header(Enum, :cuont, 1)
+    assert {:count, 2} in suggestions
   end
 end
